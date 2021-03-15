92ada91b4f2afb2530ef2dda1b5329a7ba414067
Anastasia-pro <78647195+Anastasia-pro@users.noreply.github.com>
2021-03-15 23:10:16 +0300

    init: start Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let ArrBrackets = [];
+    let ensign;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        ArrBrackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    } 
+    do {
+        ensign = false;
+      for (let k = 0; k < ArrBrackets.length; k++) {
+        if (str.indexOf(ArrBrackets[k]) !== -1) {
+          str = str.replace(ArrBrackets[k], '');
+          ensign = true;
+        }
+      }
+    }
+    while ((str.length !== 0) && (ensign !== false));
+    return ensign;
+  }
